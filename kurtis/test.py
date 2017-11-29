852cf0f53eae304babb638c39756de3fd922f466
kurtisdavid <kurtis.david@live.com>
2017-11-28 18:25:04 -0600

    gradient free should work; util added

--- kurtis/test.py
+++ kurtis/test.py
@@ -13,7 +13,8 @@ drift_actions = [0,32]
 fire_actions = [0,128]
 
 input_state_vars = ["position_along_track", "distance_to_center", "speed", "angle"]
-reward_state_vars = ["position_in_race","smooth_speed"]
+reward_state_vars = ["position_along_track","position_in_race","smooth_speed","wrongway"]
+SCORE_WEIGHT = [1,.1, .5, -.5]
 
 def play_level(policy, K, **kwargs):
     K.restart()
@@ -39,11 +40,12 @@ def play_level(policy, K, **kwargs):
     start = state['timestamp']
 
     
-    best_progress, idle_step = 0, 0
-    while state['timestamp']-start<30:
+    best_progress, idle_step = -100, 0
+    rescue = False
+    total_idle = 0
+    while state['timestamp']-start<60000:
         A = policy(Is[-1], Ss[-1], **kwargs) | default # feed in last image and state, OR result with accelerate
         As.append(A)
-        default = 4
         step = K.step(A)
         while not step:
             step = K.step(A)
@@ -51,22 +53,32 @@ def play_level(policy, K, **kwargs):
         Is.append(1*obs)
         Rs.append([[state[s] for s in reward_state_vars]])
         Ss.append([state[s] for s in input_state_vars])
+        
+        
+        if state['position_along_track']<=best_progress:
+           idle_step += 1
+           total_idle += 1
 
-        idle_step += 1
-        if idle_step > 100:
-            default = default | 64
-            idle_step = 0
-        if best_progress < state['position']:
-            best_progress = state['position']
+        if idle_step > 250:
+            break
+
+        if best_progress < state['position_along_track']:
+            best_progress = state['position_along_track']
             idle_step = 0
 
-        print("Action:",A,"\tIdle:",idle_step)
+        #print("Action:",A,"\tidle_step:",idle_step)
 
 
 
     while len(As) < len(Is):
         As.append(0)
-    return np.array(Is), np.array(Rs), np.array(As)
+    return np.array(Is), np.array(Rs), total_idle
+
+def score_policy(policy, K,**kwargs):
+    score = []
+    _,s,idle = play_level(policy,K, **kwargs)
+    score.append(s[-1])
+    return np.mean(score, axis=0),idle
 
 
 class CNNPolicy:
@@ -98,7 +110,7 @@ class CNNPolicy:
       
 
         self.steer_logit = tf.identity(steer_layer)
-        self.drift_logit = tf.identity(steer_layer)
+        self.drift_logit = tf.identity(drift_layer)
         self.fire_logit = tf.identity(fire_layer)
 
         self.predicted_steer = tf.identity(tf.argmax(self.steer_logit, axis=1), name='steer')
@@ -112,8 +124,8 @@ class CNNPolicy:
         print("Total number of variables:",np.sum([v.get_shape().num_elements() for v in tf.trainable_variables()]))
     
     def __call__(self, I, prev_state, verbose=False):
-        s,d,f = sess.run([self.predicted_steer,self.predicted_drift,self.predicted_fire], {self.I: I[None], self.state: prev_state})[0]
-        
+        s,d,f = sess.run([self.predicted_steer,self.predicted_drift,self.predicted_fire], {self.I: I[None], self.state: prev_state})
+        #print(s,d,f)
         return steer_actions[int(s)]|drift_actions[int(d)]|fire_actions[int(f)]
     
     @property
@@ -150,4 +162,59 @@ sess = tf.Session(config = config)
 sess.run(tf.global_variables_initializer())
 
 
+def f(x):
+    P.flat_weights = x
+    results,idle = score_policy(P,K)
+    return np.sum(results*SCORE_WEIGHT)+min(-1,idle*-.001)
+
+SAMPLES_PER_EPOCH = 10
+EPOCHS = 30
+SURVIVAL_RATE = 0.2
+VARIANCE_EPS = 0.1
+e = 0.001
+prob = 0.01
+init_weights = []
+scores = []
+for i in range(SAMPLES_PER_EPOCH):
+    # extract initial weights using xavier initialization
+    #tf.reset_default_graph()
+    init_weights.append(P.flat_weights + np.random.normal(0,.01,size = (P.flat_weights.shape[0])))
+    scores.append(f(init_weights[-1]))
+
+
+#print(scores)
+best = np.argmax(scores)
+P.flat_weights = init_weights[best]
+
+# print("Init max: ",scores[max_])
+# print("Calc again: ",f(P.flat_weights))
+mean = np.zeros(P.flat_weights.shape)
+std = 0*mean + 1
+
+
+# TODO: Use whichever optimization algorithm you want here
+first = P.flat_weights
+best_policies = [P.flat_weights]
+for it in range(EPOCHS):
+    individuals = np.random.normal(0,VARIANCE_EPS,size=(SAMPLES_PER_EPOCH,P.flat_weights.shape[0]))+P.flat_weights
+    for i in range(individuals.shape[0]):
+        lel = np.random.random(size=P.flat_weights.shape[0])
+        jitter = np.asarray([e for i in range(lel.shape[0])])
+        individuals[i] = individuals[i]+jitter
+    results = np.asarray([f(i) for i in individuals])
+    winner = np.argmax(results)
+    P.flat_weights = individuals[winner]
+    best_policies.append(individuals[winner])
+    print("["+str(it)+"]\t",results[winner])
+    if results[winner]>0.1:
+        util.save('final_gf'+str(results[winner])[2:]+'.tfg', session=sess)
+
+best_policies = np.asarray(best_policies)
+# who's really the best?
+results = np.asarray([f(i) for i in best_policies])
+winner = np.argmax(results)
+
+P.flat_weights = best_policies[winner]
+util.save('final_gf_winner'+str(results[winner])[2:]+'.tfg', session=sess)
+print("PLAYING FINAL")
 play_level(P,K)

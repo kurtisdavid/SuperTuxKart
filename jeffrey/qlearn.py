9fc6c5cbb940809b3cbfcd46185915e4b4439310
jeffjiang42 <jeffjiang@utexas.edu>
2017-11-30 15:54:49 -0600

    qlearning

--- jeffrey/qlearn.py
+++ jeffrey/qlearn.py
@@ -30,7 +30,7 @@ class AI:
             action += 1
         elif (state['angle']<-1.5):
             action += 1
-        
+
         #if(abs(state['angle']) > 0.5):
          #   action += 32
         #print(action)
@@ -45,7 +45,7 @@ class QFunc:
 
         #input labels going to be the Q values we precomputed
         self.labels = tf.placeholder(tf.float32, (None, len(VALID_ACTIONS)), name = 'labels')
-        
+
         resize = tf.image.resize_images(self.I,[64,96])
         _, std = tf.nn.moments(self.I, axes=[1])
         mu = tf.reduce_mean(self.I,1) # per pixel mean
@@ -61,11 +61,11 @@ class QFunc:
         combined = tf.concat([fc1,tf.expand_dims(self.state,0)],axis=1)
 
         fc2 = tf.contrib.layers.fully_connected(inputs = combined, num_outputs = 16,weights_regularizer = tf.nn.l2_loss)
-        
+
         self.qvalues = tf.identity(tf.contrib.layers.fully_connected(inputs = fc2, num_outputs = len(VALID_ACTIONS), weights_regularizer = tf.nn.l2_loss, activation_fn = None), name = 'qvalues')
 
         self.max_action = tf.identity(tf.argmax(input = qvalues, axis = 1), 'name' = action)
-        
+
         #self.variables =  tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
         #self.__vars_ph = [tf.placeholder(tf.float32, v.get_shape()) for v in self.variables]
         #self.__assign = [v.assign(p) for v,p in zip(self.variables, self.__vars_ph)]
@@ -85,18 +85,17 @@ class QFunc:
         qvalue, qaction = sess.run( [self.qvalues, self.max_action], {self.I: image, self.state: state})
         return qvalue, qaction
 
-    
-Q = QFunc()
-K.restart()
-K.waitRunning()
 
-NUM_STEPS = 10000
+Q = QFunc()
+
+
+NUM_EPOCHS = 100
 EPSILON = 0.01
 GAMMA = 0.5
 BATCH_SIZE = 32
+NEW_DATA_SIZE = 1000
 
 prev_state = None
-prev_action = None
 prev_obs = None
 state = None
 obs = None
@@ -105,45 +104,84 @@ action = 4
 i = False
 last_time = 0
 
-#Training the Q function
-for i in range(NUM_STEPS):
-    step = K.step( action )
-    i = not i
-    if step:
-        #Collect some data
-        if state is not None:
-            prev_state = state
-            prev_action = action
-            prev_obs = obs
+#Pull the data
+prev_obs_memory = np.load('small_prev_obs_memory')
+prev_r = np.load('prev_r')
+obs_memory = np.load('small_obs')
+prev_state_memory = np.load('prev_state_memory')
+state_memory = np.load('state_memory')
 
-        state, obs = step
-        state['position_along_track'] = state['position_along_track'] % 1
-        if(np.random.rand() < EPSILON):
-            #choose a random action
-            action = random.choice(VALID_ACTIONS)
-        else:
-            _, action = Q(obs, state)
-        if i:
-            action += 128
+for e in range(NUM_EPOCHS):
+    print("EPOCH: " + str(e))
+    K = Kart("lighthouse", 300, 200)
+    K.restart()
+    K.waitRunning()
+
+    new_prev_obs = []
+    new_r = []
+    new_obs = []
+    new_prev_state = []
+    new_state = []
+    #Gather data
+    while(len(new_state) < NEW_DATA_SIZE):
+        step = K.step( action )
+        if step:
+            if state is not None:
+                prev_state = state
+                prev_obs = obs
+            state, obs = step
+            state['position_along_track'] = state['position_along_track'] % 1 # only care about distance in race?
+
+            #Random action
+            if np.random.rand() < EPSILON:
+                 action = VALID_ACTIONS[np.random.randint(0, len(VALID_ACTIONS))]
+            else:
+                #Otherwise, pick maximal Q action
+                action = Q(obs, state)
 
         if step and time.time()-last_time > 1/750:
             last_time = time.time()
             if prev_state is not None:
                 reward = state['position_along_track'] - prev_state['position_along_track']
-                memory.append((prev_state, prev_obs, prev_action, reward, state, obs))
+                new_prev_obs.append(prev_obs)
+                new_obs.append(obs)
+                new_r.append(reward)
+                new_prev_state.append(prev_state)
+                new_state.append(state)
 
-        #Train one batch
-        sample = np.random.choice(memory, size = BATCH_SIZE)
-        #used to compute labels
-        label_states = [s[0] for s in sample]
-        label_images = [s[1] for s in sample]
-        label_reward = [s[3] for s in sample]
+    #add new data to dataset
+    prev_obs_memory.extend(new_prev_obs)
+    prev_r.extend(new_r)
+    obs_memory.extend(new_obs)
+    prev_state_memory.extend(new_prev_state)
+    state_memory.extend(new_state)
+    K.quit()
+    #Train now
+    sample_indices = np.random.choice(range(len(prev_r)), BATCH_SIZE)
 
-        train_states = [s[4] for s in sample]
-        train_images = [s[5] for s in sample]
+    sample_prev_obs = [prev_obs_memory[i] for i in sample_indices]
+    sample_r = [prev_r[i] for i in sample_indices]
+    sample_obs = [obs_memory[i] for i in sample_indices]
+    sample_prev_state = [prev_state_memory[i] for i in sample_indices]
+    sample_state = [state_memory[i] for i in sample_indices]
 
-        label_qvals = label_reward + GAMMA * Q(label_images, label_states)
-        Q.train_batch(train_images, train_states, train_reward, label_qvals)
+    qval_label, _ = Q(sample_obs, sample_state)
+    labels = sample_r + GAMMA * qval_label
+    Q.train_batch(sample_obs, sample_state, labels)
+
+
+
+
+
+
+
+
+
+
+
+#Training the Q function
+# High level idea: Gather data for some time
+# Then grab a batch from the data set and train
 
 
 
